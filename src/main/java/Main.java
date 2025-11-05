47f0600a142e239d9e0420e24f9f5eba7f18b343
heliguy4599 <heliguy4599@hotmail.com>
2025-11-04 20:19:47 -0500

    Format and comment Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,33 @@
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java Main <source file>");
-            System.exit(1);
-        }
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("Usage: java Main <source file>");
+			System.exit(1);
+		}
 
-        String filePath = args[0];
+		String filePath = args[0];
 
-        try {
-            CharStream charStream = CharStreams.fromFileName(filePath);
+		try {
+			CharStream charStream = CharStreams.fromFileName(filePath);
 
-	    // Lexer
-            AntlerScriptLexer lexer = new AntlerScriptLexer(charStream);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
+			// Lexer
+			AntlerScriptLexer lexer = new AntlerScriptLexer(charStream);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-	    // Parser
-            AntlerScriptParser parser = new AntlerScriptParser(tokens);
-            ParseTree tree = parser.program();
+			// Parser
+			AntlerScriptParser parser = new AntlerScriptParser(tokens);
+			ParseTree tree = parser.program();
 
-	    // AST
-            AntlerScriptVisitor visitor = new AntlerScriptVisitor();
-            visitor.visit(tree);
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-        }
-    }
+			// AST
+			AntlerScriptVisitor visitor = new AntlerScriptVisitor();
+			visitor.visit(tree); // Runs the code
+		} catch (IOException e) {
+			System.err.println("Error reading file: " + e.getMessage());
+		}
+	}
 }
